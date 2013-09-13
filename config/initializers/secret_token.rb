56e0dde73a7934f683c80640b8fa76d181ef2ee5
Stephen Kennedy <skennedy_cw@sharecare.com>
2013-09-12 21:17:00 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JakeApp::Application.config.secret_key_base = '17c76efa764b75c02d2d3d793b2ff8d50f20c71a0f36f22343fe893eae17cf3345158e4cdcc337b475b37cfd1c482481f88a77ea005192f01a17d1e97a9facf2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JakeApp::Application.config.session_store :cookie_store, key: '_jake_app_session'
